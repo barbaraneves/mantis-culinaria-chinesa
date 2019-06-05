24453f457c7e0605cc1c70708702e4b743cc2849
Bárbara Neves <barbara.steph@alu.ufc.br>
2019-06-05 00:43:46 -0300

    Começando o Back-End da aplicação

---
+++ MantisCulinariaChinesa/src/main/java/mantis/com/br/MantisCulinariaChinesaApplication.java
@@ -0,0 +1,13 @@
+package mantis.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantisCulinariaChinesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantisCulinariaChinesaApplication.class, args);
+	}
+
+}
